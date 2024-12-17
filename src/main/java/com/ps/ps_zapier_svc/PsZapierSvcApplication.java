bfbcd3e2ce357a5683fece65adbd89ba0a44d755
saravanan.m@purpleslate.in <saravanan.m@purpleslate.in>
2024-12-17 10:55:21 +0530

    Initial commit

---
+++ src/main/java/com/ps/ps_zapier_svc/PsZapierSvcApplication.java
@@ -0,0 +1,13 @@
+package com.ps.ps_zapier_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsZapierSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsZapierSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/ps_zapier_svc/PsZapierSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.ps_zapier_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsZapierSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
